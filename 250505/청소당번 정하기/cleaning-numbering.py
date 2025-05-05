05b29f222f5b8317570ea0898c8134e98334e36c
hojunlee123 <86946813+hojunlee123@users.noreply.github.com>
2025-05-05 23:16:21 +0900

    Title: 청소당번 정하기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250505/청소당번 정하기/cleaning-numbering.py
+++ 250505/청소당번 정하기/cleaning-numbering.py
@@ -1,15 +1,15 @@
 n = int(input())
 
-classroom_cleaning = 0
-hallway_cleaning = 0
-bathroom_cleaning = 0
+classroom = 0  
+hallway = 0   
+bathroom = 0   
 
 for day in range(1, n + 1):
     if day % 12 == 0:
-        bathroom_cleaning += 1
-    elif day % 2 == 0:
-        classroom_cleaning += 1
+        bathroom += 1
     elif day % 3 == 0:
-        hallway_cleaning += 1
+        hallway += 1
+    elif day % 2 == 0:
+        classroom += 1
 
-print(classroom_cleaning, hallway_cleaning, bathroom_cleaning)
+print(classroom, hallway, bathroom)
